c26c78e8bc56d3a78dc58e65fd8fa88aff0a0077
pppatil7 <pundalikpatil7218@gmail.com>
2024-09-21 10:57:25 +0530

    ds sample

---
+++ code.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+int a=10;
+cout<<a++<<endl; //10
+cout<<++a<<endl; //12
+
+cout<<a--<<endl; //12 
+cout<<--a<<endl; //10
+
+
+
+
+
+
+
+// int a,b;
+// cout<<"enter a: ";
+// cin>>a;
+
+// cout<<"enter b: ";
+// cin>>b;
+
+// int sum=a+b;
+// cout<<"sum= "<<sum<<endl;
+
+
+//    cout<<(1<3)<<endl;
+//    cout<<(5>3)<<endl;
+//    cout<<(3<=3)<<endl;
+//    cout<<(3!=3)<<endl;
+//    cout<<(2==2)<<endl;
+
+    // cout<<!(2==2)<<endl;
+    // cout<<((2==2) || (1>2))<<endl;
+    // cout<<((2==2) && (1>2))<<endl;
+
+
+
+    // int a=5;
+    // double b=2;
+    // int ans = (15/(double)2);
+
+    // cout<<(a/b)<<endl;
+    // cout<<(15/(double)2)<<endl;
+    // cout<<ans<<endl;
+
+    //  int a = 12, b = 5;
+    //  int sum = a + b;
+    //  cout << sum <<endl;
+
+    //  cout<<"sum is: "<<(a+b)<<endl;
+    //  cout<<"difference is: "<<(a-b)<<endl;
+    //  cout<<"product is: "<<(a*b)<<endl;
+    //  cout<<"division is: "<<(a/b)<<endl;
+    //  cout<<"modulo is: "<<(a%b)<<endl;
+
+    // int marks;
+    // cout<<"Enter the Marks: ";
+    // cin>>marks;
+
+    // cout<<"your marks is: "<<marks<<endl;
+
+    // int age = 20 ;
+    // char grade = 'A';
+    // float PI = 3.14f;
+
+    // bool isSafe = false;
+    // double price = 200.45;
+    
+    // int value = grade;
+
+    // int newPrice = (int) price;
+
+    //cout << age <<endl;
+    //cout << grade <<endl;
+    //cout << PI <<endl;
+
+    //cout<< isSafe << endl;
+    //cout<< price <<endl;
+    //cout<<value<<endl;
+
+    // cout<<newPrice<<endl;
+    return 0;
+}
