2e77ecb1038b5aab4ea7e0445fd6ff0b45109b5a
pppatil7 <pundalikpatil7218@gmail.com>
2024-09-21 12:01:49 +0530

    condition stat

---
+++ conditionstat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = -12;
+
+    if(number>=0){
+      cout<<"number is positive\n";
+    }else{
+        cout<<"number is negative\n";
+    }
+
+    return 0;
+}
