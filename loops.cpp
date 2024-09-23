76a714d06454f3e920ca84f5052344b6ac4b9d04
pppatil7 <pundalikpatil7218@gmail.com>
2024-09-23 15:54:00 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+int number = 10;
+int i = 1;
+
+do{
+    cout << i <<" ";
+    i++;
+}while(i<=number);
+
+// int number = 10;
+// int oddSum = 0;
+
+
+// for(int i=1;i<=number;i++){
+//     if(i%2!=0){
+//         cout<<i<<" ";
+//         oddSum=oddSum+i;
+//     }
+// }
+
+// cout<<"oddSum is: "<<oddSum<<endl;
+
+// int number = 10;
+// int sum = 0;
+// int i = 1;
+
+// while(i<=number){
+// sum=sum+i;
+// if(i==5){
+// break;
+// }
+// i++;
+// }
+
+// cout << "sum is :" << sum << endl;
+
+// int number = 10;
+// int sum = 0;
+
+// for(int i=1;i<=number;i++){
+// sum=sum+i;
+// }
+
+// cout << "sum is :" << sum << endl;
+
+
+// int number = 10;
+
+// for(int i=0; i<number ;i++){
+// cout<< i << " ";
+// }
+
+
+
+
+// int n= 50;
+// int i = 1;
+
+// while(i<=n){
+// cout <<i << " ";
+// i++;
+// }
+// cout << endl;
+
+//       int count=1;
+     
+// while(count<=10){
+//      cout<<count << " ";
+//      count++;
+//}
+
+
+    return 0;
+}
