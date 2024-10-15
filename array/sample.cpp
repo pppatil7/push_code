d4a87c02aabe557f02ef4b57e5ae3b0472e6fd07
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-15 11:23:32 +0530

    update

---
+++ array/sample.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int numbers[5] = {1, 3, 5, 7, 9};
+
+   double weight[] = {74.56, 23.43, 34.21, 45.32, 43.21, 65.54, 76.31, 54.21}; //8 size
+
+
+   cout << weight[0] << endl;
+   cout << weight[1] << endl;
+   cout << weight[2] << endl;
+   cout << weight[3] << endl;
+   cout << weight[4] << endl;
+   cout << weight[5] << endl;
+   cout << weight[6] << endl;
+   cout << weight[7] << endl;
+
+
+
+   cout << numbers[0] << endl;
+
+   
+
+    return 0;
+}
