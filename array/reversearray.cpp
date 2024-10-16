13c2773dcf487c0449d28f51e1c09e880ff63b6b
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-16 13:17:03 +0530

    update

--- array/reversearray.cpp
+++ array/reversearray.cpp
@@ -16,11 +16,12 @@ void reverseArr(int arr[], int size){
 int main(){
 
 int arr[] = {1,2,3,4,5,6,7,8};
-int size = 9;
+int size = 8;
 reverseArr(arr,size);
 for(int i = 0; i < size; i++){
-cout << arr[i]<<" ";
+cout << arr[i] << " ";
 }
+cout << endl;
 
     return 0;
 }
