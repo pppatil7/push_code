13f628318416518ac933168aa102cad0485ec500
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-20 07:42:01 +0530

    update

--- vector/sample.cpp
+++ vector/sample.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main(){
 
     vector<int> vec; //0
+    vector<int> vec1 = {1,2,3}; //3
+    cout << vec1[0] <<endl;
+    
+    vector<int> vec2(5,13);
+
+
+    cout << vec2[3] << endl;
+
     return 0;
 }
