198a39986f4c13e871a157657eba459087608324
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-20 07:55:47 +0530

    update

---
+++ vector/vectorfunctions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    vector vec = {'a','b','c','d','e','f'};
+
+    cout << "size= " <<vec.size() << endl;
+
+    return 0;
+}
