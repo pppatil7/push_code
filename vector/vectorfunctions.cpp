62d1003697c8df7cbd815c8d0d343bcf80671c88
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-20 10:21:33 +0530

    update

--- vector/vectorfunctions.cpp
+++ vector/vectorfunctions.cpp
@@ -4,9 +4,19 @@ using namespace std;
 
 int main(){
     
-    vector vec = {'a','b','c','d','e','f'};
+    // vector<char> vec = {'a','b','c','d','e','f'};
+    vector<char> vec1;
 
-    cout << "size= " << vec.size() << endl;
+    cout << "size= " << vec1.size() << endl;
+
+    vec1.push_back(1);
+    vec1.push_back(2);
+    vec1.push_back(4);
+    vec1.push_back(6);
+    cout << "size= " << vec1.size() << endl;
+    for(int value :vec1){
+        cout << value <<" ";
+    }
 
     return 0;
 }
