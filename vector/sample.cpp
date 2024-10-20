b7ef576b220db21b7fa9d9c1864324b5518d0f1d
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-20 07:48:19 +0530

    update

--- vector/sample.cpp
+++ vector/sample.cpp
@@ -4,14 +4,24 @@ using namespace std;
 
 int main(){
 
-    vector<int> vec; //0
-    vector<int> vec1 = {1,2,3}; //3
-    cout << vec1[0] <<endl;
+    // vector<int> vec; //0
+    // vector<int> vec1 = {1,2,3}; //3
+    // cout << vec1[0] <<endl;
     
-    vector<int> vec2(5,13);
+    // vector<int> vec2(5,13);
 
 
-    cout << vec2[3] << endl;
+    // for(int i : vec2){
+    //    cout << i <<endl;
+    // }
+
+    vector<char> vec3 = {'a','e','i','o','u'};
+
+     for(char j : vec3){
+      cout << j << endl;
+     }
+
+
 
     return 0;
 }
