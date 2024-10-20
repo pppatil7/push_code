5f1e98b25bc761bb9470a5f7c8e0c28edad916cd
pppatil7 <pundalikpatil7218@gmail.com>
2024-10-20 10:38:03 +0530

    update

--- vector/vectorfunctions.cpp
+++ vector/vectorfunctions.cpp
@@ -9,11 +9,21 @@ int main(){
 
     cout << "size= " << vec1.size() << endl;
 
-    vec1.push_back(1);
+    vec1.push_back(1); //element push at the end
     vec1.push_back(2);
     vec1.push_back(4);
     vec1.push_back(6);
     cout << "size= " << vec1.size() << endl;
+    for(int value :vec1){
+        cout << value <<" ";
+    }
+    cout<<endl;
+    
+    vec1.pop_back(); //element pop at the end
+    
+    cout << "size= " << vec1.size() << endl;
+
+
     for(int value :vec1){
         cout << value <<" ";
     }
