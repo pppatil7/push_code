db735f1489755657044d6d532496b3e53fd5a0cf
pppatil7 <pundalikpatil7218@gmail.com>
2025-01-09 13:55:54 +0530

    update

---
+++ array/arrayproblems/maxsubarray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int maxSum = INT_MIN;
+
+   for(int st=0;){
+
+   }
+
+
+
+
+    return 0;
+}
--- array/arrayproblems/subarray.cpp
+++ array/arrayproblems/subarray.cpp
@@ -6,12 +6,12 @@ int main(){
     int n = 5;
     int arr[5] = {1,2,3,4,5};
 
-    for(int start=0;start<n;start++){
+    for(int start= 0;start<n;start++){
         for(int end = start;end < n;end++){
             for(int i = start;i <= end;i++){
               cout << arr[i];
             }
-            cout << " "; 
+            cout << " ";
         }
         cout << endl;
     }
